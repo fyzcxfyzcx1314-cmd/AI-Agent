c4a881414c5085c1682275c131c2db59215dfebc
fyzcx <fyzcxfyzcx1314@gmail.com>
2025-12-23 16:20:59 +0800

    更多解析器

--- RAG/LongChain/模型开发/结果解析.py
+++ RAG/LongChain/模型开发/结果解析.py
@@ -6,3 +6,18 @@ result = 0
 parser = StrOutputParser()
 ### 2. 提取
 invoke = parser.invoke(result)
+
+
+### 其他类型的解析器
+
+#### 1. CSV解析器
+from langchain_core.output_parsers import CommaSeparatedListOutputParser
+csv_parser = CommaSeparatedListOutputParser()
+
+#### 2. json解析器
+from langchain_core.output_parsers import JsonOutputParser
+json_parser = JsonOutputParser()
+
+#### 3. 日期时间解析器
+from langchain.output_parsers import DatetimeOutputParser
+data_parser = DatetimeOutputParser()
